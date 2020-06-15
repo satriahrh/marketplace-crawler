e69e6e21ce11c61de4f8400074dae16d1f8fea20
Satria H R Harsono <satriahrh@gmail.com>
2020-06-15 21:27:08 +0700

    add get item

--- services/shopee.rb
+++ services/shopee.rb
@@ -1,10 +1,12 @@
 require_relative 'shopee/00_base'
+require_relative 'shopee/get_item'
 require_relative 'shopee/search_items'
 
 module Services
   module Shopee
     module_function
 
+    def get_item(*args); Services::Shopee::GetItem.new(*args).call; end
     def search_items(*args); Services::Shopee::SearchItems.new(*args).call; end
   end
 end
